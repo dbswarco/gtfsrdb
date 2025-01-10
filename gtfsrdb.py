dcf1cfce376ebee7a2c948f0d88022e4c2a84d36
Dan.Brandesky <dan.brandesky@swarco.com>
2025-01-10 14:17:30 -0500

    print debug again

--- gtfsrdb.py
+++ gtfsrdb.py
@@ -118,6 +118,7 @@ if opts.vehiclePositions is None:
 if opts.header is not None:
     print(opts.header)
     opts.header = dict(item.split("=") for item in opts.header.split(","))
+    print(opts.header)
     #opts.header = json.loads(opts.header)
 
 # Connect to the database
