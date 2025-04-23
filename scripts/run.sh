208402d674725cb2ee3454411d72c5e05e04df55
dbswarco <dan.brandesky@swarco.com>
2025-04-23 14:44:39 -0400

    Improve run script to accept gtfs feeds from a txt file

--- scripts/run.sh
+++ scripts/run.sh
@@ -10,6 +10,18 @@ fi
 DB_NAME="$1"
 shift
 
-echo "select 'create database $1' where not exists (select from pg_database where datname = '$DB_NAME')\gexec" | \
+if [[ "$1" == "-I" ]]; then
+  gtfsrdb_args=$(cat "$2")
+  shift 2
+  optional_args=$*
+else
+  gtfsrdb_args=$*
+  optional_args=''
+fi
+
+echo "select 'create database $DB_NAME' where not exists (select from pg_database where datname = '$DB_NAME')\gexec" | \
   psql -d "$DB" -h "$DB_HOST" -U "$DB_USER"
-python3 "$PROJECT_DIR"/src/gtfsrdb/gtfsrdb.py -d postgresql+psycopg2://"$DB_USER":"$DB_PASS"@"$DB_HOST"/"$DB_NAME" -c "$@"
+# Uncomment to debug commands sent to gtfsrdb.py.
+# set -x
+python3 "$PROJECT_DIR"/src/gtfsrdb/gtfsrdb.py -d postgresql+psycopg2://"$DB_USER":"$DB_PASS"@"$DB_HOST"/"$DB_NAME" -c \
+  $gtfsrdb_args $optional_args
