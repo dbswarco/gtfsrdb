36c0d4da5b2b850ed1e62173c66e6fdd4e683b57
dbswarco <dan.brandesky@swarco.com>
2025-04-23 14:51:57 -0400

    Catch SQL errors and exit run script if errors occurred

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,7 +3,8 @@
 source env.sh
 
 if [ $# -eq 0 ]; then
-    >&2 echo "Arguments: [agency] [-t trip feed] [-p vehicle/position feed] [-a alerts feed]"
+    >&2 echo "Arguments: [agency] [[-t <trip feed url>] [-p <vehicle/position feed url>] [-a <alerts feed url>]] | \
+    [-I <path to txt file with gtfs feeds, one per line>]] [optional arguments for gtfsrdb]"
     exit 1
 fi
 
@@ -21,6 +22,13 @@ fi
 
 echo "select 'create database $DB_NAME' where not exists (select from pg_database where datname = '$DB_NAME')\gexec" | \
   psql -d "$DB" -h "$DB_HOST" -U "$DB_USER"
+
+if ! [ $? -eq 0 ]; then
+  >&2 echo "PostgreSQL command failed. Check values in env.sh for accuracy and ensure that PostgreSQL server is \
+  running and accepting connections."
+  exit 2
+fi
+
 # Uncomment to debug commands sent to gtfsrdb.py.
 # set -x
 python3 "$PROJECT_DIR"/src/gtfsrdb/gtfsrdb.py -d postgresql+psycopg2://"$DB_USER":"$DB_PASS"@"$DB_HOST"/"$DB_NAME" -c \
