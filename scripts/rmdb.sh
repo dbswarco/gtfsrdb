b6212b718ebf80e1f323cc8ae9e5100487fc2c7f
dbswarco <dan.brandesky@swarco.com>
2025-04-23 14:23:25 -0400

    improve scripts: create scripts directly, use env.sh to set up variables for scripts, improve bash command line arg pass through

--- scripts/rmdb.sh
+++ scripts/rmdb.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+source env.sh
+
 if [ $# -eq 0 ]; then
     >&2 echo "Arguments: [agency database to remove]"
     exit 1
 fi
 
-psql -d ott -h 10.5.0.2 -U ott -c "drop database $1;"
+psql -d "$DB" -h "$DB_HOST" -U ott -c "drop database $1;"
